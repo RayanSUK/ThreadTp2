8818c68230af1ab0ba6aeecd9d085ee0ab983b14
Rayan <sukkarie.rayan@gmail.com>
2025-11-09 19:23:00 +0100

    partie entre/sorti session critique

--- src/Main.java
+++ src/Main.java
@@ -8,17 +8,29 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Affichage TA = new Affichage("AAA");
-		Affichage TB = new Affichage("BB");
-		Affichage TC = new Affichage("CCCC");
-		Affichage TD = new Affichage("DDD");
+		semaphoreBinaire sem = new semaphoreBinaire(1);
+
+		Thread TA = new Thread(() -> imprimer("AAA", sem));
+		Thread TB = new Thread(() -> imprimer("BB",  sem));
 
 		TA.start();
-		TC.start();
 		TB.start();
-		TD.start();
+
 
 
 	}
 
+	static void imprimer(String texte, semaphoreBinaire sem) {
+		sem.syncWait();                             // Wait() : prendre le verrou
+		try {
+			System.out.println("j'entre en section critique");
+			for (int i=0; i<texte.length(); i++) {
+				System.out.print(texte.charAt(i));
+				try { Thread.sleep(100); } catch (InterruptedException e) {}
+			}
+			System.out.println("\nje sors de section critique");
+		} finally {
+			sem.syncSignal();                         // Signal() : libérer le verrou
+		}
+	}
 }
